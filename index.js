2df62aa7b8fe9498c9c0efdc7c834f13c50a7286
Wing Yeung Leung <kelvinlwy2002@yahoo.com.hk>
2019-03-28 22:06:24 +1000

    Solution

--- index.js
+++ index.js
@@ -4,10 +4,73 @@
  * @returns {string}
  */
 exports.example = () => 'hello world';
-exports.stripPrivateProperties = () => {};
-exports.excludeByProperty = () => {};
-exports.sumDeep = () => {};
-exports.applyStatusColor = () => {};
-exports.createGreeting  = () => {};
-exports.setDefaults = () => {};
-exports.fetchUserByNameAndUsersCompany  = () => {};
+
+exports.stripPrivateProperties = (props, arr) => {
+  return arr.map((item) => {
+    props.forEach((prop) => {
+      if (item[prop]) {
+        delete item[prop];
+      }
+    });
+    return item;
+  });
+};
+
+exports.excludeByProperty = (prop, arr) => {
+  return arr.filter(item => item[prop] === undefined);
+};
+
+exports.sumDeep = (arr) => {
+  return arr.map(item => {
+    const key = Object.keys(item)[0];
+
+    const sum = item.objects.reduce((acc, cur) => {
+      return acc + cur.val;
+    }, 0);
+
+    return {[key]: sum};
+  });
+};
+
+exports.applyStatusColor = (colorMap, arr) => {
+  return arr.map(item => {
+    const color = Object.keys(colorMap).find(key => {
+      return colorMap[key].includes(item.status);
+    });
+
+    return {
+      ...item,
+      color
+    }
+  }).filter(status => {
+    return status.color !== undefined
+  });
+};
+
+exports.createGreeting = (greet, greeting) => {
+  return name => greet(greeting, name);
+};
+
+exports.setDefaults = defaultProps => props => {
+  let newObject = Object.assign({}, props);
+
+  Object.keys(defaultProps).forEach(prop => {
+    if (newObject[prop] === undefined) {
+      newObject[prop] = defaultProps[prop];
+    }
+  });
+  return newObject;
+};
+
+exports.fetchUserByNameAndUsersCompany = async (name, services) => {
+  const status = await services.fetchStatus();
+  const users = await services.fetchUsers();
+
+  const user = users.find(item => {
+    return item.name.localeCompare(name) === 0;
+  });
+
+  const company = await services.fetchCompanyById(user.companyId);
+
+  return {status, user, company};
+};
